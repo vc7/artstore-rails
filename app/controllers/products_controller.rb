54ff837ffab762187e449a9d49948e7c5c97c5cf
Vincent Chen <vince78718@gmail.com>
2015-09-14 19:46:19 +0800

    加入購物車中是否存在的邏輯。原本在加入購物車前，並沒有判斷是不是已經有加過了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,15 @@ class ProductsController < ApplicationController
 
 	def add_to_cart
 		@product = Product.find(params[:id])
-		current_cart.add_product_to_cart(@product)
+
+		# 要判斷購物車裡面有沒有這項物品，才看是否需要加入
+		if !current_cart.items.include?(@product)
+			current_cart.add_product_to_cart(@product)
+			flash[:notice] = "你已經成功把 #{@product.title} 加入購物車"
+		else
+			flash[:warning] = "你的購物車已經有此商品"
+		end
+
 		redirect_to :back
 	end
 end
